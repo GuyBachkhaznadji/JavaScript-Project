bfa9372a0a584ea1f61b4885e6e44a60c18a535c
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-10-30 21:46:49 +0000

    amended urls

--- client/src/app.js
+++ client/src/app.js
@@ -11,7 +11,7 @@ var app = function(){
     welcomePopUpWindow.render();
 
 
-    var favouritesData = new AjaxRequest('http://localhost:3000/favourites');
+    var favouritesData = new AjaxRequest('/favourites');
     var favouritesTag = document.querySelector("#favourites");
     var favouritesView = new FavouritesView(favouritesTag);
     favouritesData.get(favouritesView.render.bind(favouritesView));
@@ -19,7 +19,7 @@ var app = function(){
     var detailsTag = document.querySelector("#bootcamp-details");
     var bootcampDetailsView = new BootcampDetailsView(detailsTag);
 
-    var bootcampsData = new AjaxRequest('http://localhost:3000/api/code-bootcamps')
+    var bootcampsData = new AjaxRequest('/api/code-bootcamps')
     var main = document.querySelector("#all-bootcamps");
 
 
